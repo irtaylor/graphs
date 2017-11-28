ca04ee0ffa6c174c84e7b940322298f4ca2bc1cb
Ian R. Taylor <iantay@upenn.edu>
2017-11-28 12:11:02 -0500

    Add a todo

--- weighted_graph.cpp
+++ weighted_graph.cpp
@@ -16,7 +16,7 @@ typedef pair<int, int> int_pair;
 
 weighted_graph::weighted_graph(int num_v) {
   size = num_v;
-  adj = new vector<pair<int,int> >[num_v]; // pair.first is the vertex, pair.second is the edge weight
+  adj = new vector<int_pair >[num_v]; // pair.first is the vertex, pair.second is the edge weight
 }
 
 weighted_graph::~weighted_graph() {
@@ -30,22 +30,21 @@ void add_edge(weighted_graph* wg, int s, int v, int weight) {
 
 // see: http://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-using-priority_queue-stl/
 void dijkstras_shortest_path(weighted_graph *wg, int s) {
-  // TODO: I should probably make a custom comparator for pq, since i want the comparison to occur ONLY 
-  // on the weight part of the pair. 
-  priority_queue< int_pair, vector <int_pair> , greater<int_pair> > pq; // http://www.geeksforgeeks.org/implement-min-heap-using-stl/
-  vector<int> distances(wg->size, INF);
+  // TODO: I should probably have a custom comparator for pairs, since i want only the
+  // weights compared, and the comparison may accidentally happen on vertex ids: http://en.cppreference.com/w/cpp/utility/pair/operator_cmp
+  priority_queue< int_pair, vector <int_pair> , greater<int_pair> > pq; // min-heap of vertices processed so far. see http://www.geeksforgeeks.org/implement-min-heap-using-stl/
+  vector<int> distances(wg->size, INF); // computed shortest path distances.
   pq.push(make_pair(s, 0));
   distances[s] = 0;
-
+  // NOTE: the Coursera course checks pq != wg->size, probably just keep growing pq without popping the top
   while(!pq.empty()) {
     int v = pq.top().first;
-    pq.pop();
     for(auto i = wg->adj[v].begin(); i != wg->adj[v].end(); i += 1) {
       int u = i->first;
       int wgt = i->second;
 
       cout << u << ", "  << wgt << endl;
-      if(distances[u] > distances[v] + wgt) {
+      if(distances[u] > distances[v] + wgt) { // "Dijkstra's Greedy Criterion"
         distances[u] = distances[v] + wgt;
         pq.push(make_pair(u, distances[u]));
       }
